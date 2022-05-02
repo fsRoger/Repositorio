db74a0ede08ac743c7b2d166521e76c4654fc571
Roger Ferreira Da Silva <74381457+fsRoger@users.noreply.github.com>
2022-05-02 10:12:38 -0300

    add up

--- js/data-dinamica.js
+++ js/data-dinamica.js
@@ -1,8 +1,3 @@
-<script>
-				//objeto que precisa ser instanciado 
-			var data = new Date()
-			data
-			//a partir do metodo getDate(, da instancia new Date() 
-			document.write (data.getDate() + '/' + (data.getMonth() + 1) + '/dataaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa k' + data.getFullYear())
-
-	</script>
+function carregaJogo() {
+	alert('estouaki')
+}
